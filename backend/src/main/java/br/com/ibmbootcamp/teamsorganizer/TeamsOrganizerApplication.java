5c2f7e8449bc9a7c817e77183166316e5587d6ed
Marco Haiat <marco.haiat@gmail.com>
2023-09-17 11:15:25 -0300

    feat: initial project structure - root and backend directory created

---
+++ backend/src/main/java/br/com/ibmbootcamp/teamsorganizer/TeamsOrganizerApplication.java
@@ -0,0 +1,13 @@
+package br.com.ibmbootcamp.teamsorganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamsOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamsOrganizerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/ibmbootcamp/teamsorganizer/TeamsOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ibmbootcamp.teamsorganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamsOrganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
