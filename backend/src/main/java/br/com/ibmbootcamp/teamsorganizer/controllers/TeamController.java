cded9c57f80595ac9a353b8a68be247a49dc22e9
Marco Haiat <marco.haiat@gmail.com>
2023-09-18 20:04:21 -0300

    fix: team controller unnused methods commented

--- backend/src/main/java/br/com/ibmbootcamp/teamsorganizer/controllers/TeamController.java
+++ backend/src/main/java/br/com/ibmbootcamp/teamsorganizer/controllers/TeamController.java
@@ -1,12 +1,9 @@
 package br.com.ibmbootcamp.teamsorganizer.controllers;
 
 import br.com.ibmbootcamp.teamsorganizer.controllers.dtos.TeamDto;
-import br.com.ibmbootcamp.teamsorganizer.entities.PlayerEntity;
 import br.com.ibmbootcamp.teamsorganizer.entities.TeamEntity;
-import br.com.ibmbootcamp.teamsorganizer.services.PlayerService;
 import br.com.ibmbootcamp.teamsorganizer.services.TeamService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.relational.core.sql.In;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,8 +19,14 @@ public class TeamController {
   @Autowired
   private TeamService teamService;
 
+  @GetMapping
+  public ResponseEntity<Map<String, List<String>>> getAllTeams() {
+    List<TeamEntity> allTeams = teamService.getAllTeams();
+    Map<String, List<String>> allTeamsView = TeamDto.mapTeamListToTeamsView(allTeams);
+    return ResponseEntity.status(HttpStatus.OK).body(allTeamsView);
+  }
 
-  @PostMapping
+  /*@PostMapping
   public ResponseEntity<Void> insertTeam(TeamEntity team) {
     teamService.insertOrUpdateTeam(team);
     return ResponseEntity.status(HttpStatus.CREATED).body(null);
@@ -32,12 +35,5 @@ public class TeamController {
   @DeleteMapping("/{teamId}")
   public void deleteTeam(@PathVariable Integer teamId) {
     teamService.deleteTeamById(teamId);
-  }
-
-  @GetMapping
-  public ResponseEntity<Map<String, List<String>>> getAllTeams() {
-    List<TeamEntity> allTeams = teamService.getAllTeams();
-    Map<String, List<String>> allTeamsView = TeamDto.mapTeamListToTeamsView(allTeams);
-    return ResponseEntity.status(HttpStatus.CREATED).body(allTeamsView);
-  }
+  }*/
 }
