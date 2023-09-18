0c3aaf3d04683db0f04012d778decd02a4e0aee0
Marco Haiat <marco.haiat@gmail.com>
2023-09-18 20:03:05 -0300

    feat: (backend) team and player services db error treated

--- backend/src/main/java/br/com/ibmbootcamp/teamsorganizer/services/PlayerService.java
+++ backend/src/main/java/br/com/ibmbootcamp/teamsorganizer/services/PlayerService.java
@@ -1,5 +1,6 @@
 package br.com.ibmbootcamp.teamsorganizer.services;
 
+import br.com.ibmbootcamp.teamsorganizer.entities.exceptions.DbException;
 import br.com.ibmbootcamp.teamsorganizer.entities.PlayerEntity;
 import br.com.ibmbootcamp.teamsorganizer.repositories.PlayerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,14 +15,27 @@ public class PlayerService {
   private PlayerRepository playerRepository;
 
   public void insertPlayer(PlayerEntity player) {
-    playerRepository.save(player);
+    try {
+      playerRepository.save(player);
+    } catch (RuntimeException exception) {
+      throw new DbException();
+    }
+
   }
 
   public void deletePlayers() {
-    playerRepository.deleteAll();
+    try {
+      playerRepository.deleteAll();
+    } catch (RuntimeException exception) {
+      throw new DbException();
+    }
   }
 
   public List<PlayerEntity> getAllPlayers() {
-    return playerRepository.findAll();
+    try {
+      return playerRepository.findAll();
+    } catch (RuntimeException exception) {
+      throw new DbException();
+    }
   }
 }
--- backend/src/main/java/br/com/ibmbootcamp/teamsorganizer/services/TeamService.java
+++ backend/src/main/java/br/com/ibmbootcamp/teamsorganizer/services/TeamService.java
@@ -1,6 +1,6 @@
 package br.com.ibmbootcamp.teamsorganizer.services;
 
-import br.com.ibmbootcamp.teamsorganizer.entities.PlayerEntity;
+import br.com.ibmbootcamp.teamsorganizer.entities.exceptions.DbException;
 import br.com.ibmbootcamp.teamsorganizer.entities.TeamEntity;
 import br.com.ibmbootcamp.teamsorganizer.repositories.TeamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,71 +15,26 @@ public class TeamService {
   private TeamRepository teamRepository;
 
   public List<TeamEntity> getAllTeams() {
-    return teamRepository.findAll();
+    try {
+      return teamRepository.findAll();
+    } catch (RuntimeException exception) {
+      throw new DbException();
+    }
   }
 
-  public void deleteAllTeams() {
+  public void insertOrUpdateTeam(TeamEntity team) {
+    try {
+      teamRepository.save(team);
+    } catch (RuntimeException exception) {
+      throw new DbException();
+    }
+  }
+
+  /*public void deleteAllTeams() {
     teamRepository.deleteAll();
   }
 
   public void deleteTeamById(Integer teamId) {
     teamRepository.deleteById(teamId);
-  }
-
-  public void insertOrUpdateTeam(TeamEntity team) {
-    teamRepository.save(team);
-  }
-
-  /*private HashMap<String, List<String>> alocarEmTime(HashMap<String, List<String>> times, String jogador) {
-    if (times.isEmpty()) {
-      times.put("1", List.of(jogador));
-      return times;
-    }
-    boolean alocado = false;
-
-    for (Map.Entry<String, List<String>> time : times.entrySet()) {
-      if (podeAlocarNoTime(time.getValue(), jogador)) {
-        var novosIntegrantes =  new ArrayList<String>(time.getValue());
-        novosIntegrantes.add(jogador);
-        time.setValue(novosIntegrantes);
-        alocado = true;
-        break;
-      }
-    }
-    if (!alocado) {
-      String nomeNovoTime = getNewTeamName();
-      times.put(nomeNovoTime, List.of(jogador));
-    }
-    return times;
-  }
-
-  private static boolean podeAlocarNoTime(List<String> integrantes, String novoJogador) {
-    boolean result = false;
-    String sobrenomeNovoJogador = pegarSobrenome(novoJogador);
-    for (String integrante : integrantes) {
-      String sobrenomeIntegrante = pegarSobrenome(integrante);
-      if (sobrenomeIntegrante.equals(sobrenomeNovoJogador)) {
-        return false;
-      }
-      if (sobrenomeIntegrante.charAt(0) == sobrenomeNovoJogador.charAt(0)) {
-        result = true;
-      }
-    }
-    return result;
-  }
-
-  private static String pegarSobrenome(String jogador) {
-    List<String> nomeEsobrenomes = Arrays.asList(jogador.split(" "));
-    String ultimoSobrenome = nomeEsobrenomes.get(nomeEsobrenomes.size() - 1);
-    return ultimoSobrenome;
-  }
-
-  private String getNewTeamName() {
-    String lastTeam = teamRepository.getTeams().keySet().stream()
-            .sorted()
-            .reduce((first, second) -> second).get();
-
-    String newTeamName = String.valueOf(Integer.valueOf(lastTeam) + 1);
-    return newTeamName;
   }*/
 }
